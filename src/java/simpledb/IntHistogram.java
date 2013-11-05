2dbac9a9f48f49d8e490748325a56dea9d06b301
Yan Zhao <zhaoyan1117@gmail.com>
2013-11-04 19:52:33 -0800

    Exercise 2 tests passed.

--- src/java/simpledb/IntHistogram.java
+++ src/java/simpledb/IntHistogram.java
@@ -4,6 +4,12 @@ package simpledb;
  */
 public class IntHistogram {
 
+    private int[] histogram;
+    private int min;
+    private int max;
+    private int width;
+    private int ntups;
+
     /**
      * Create a new IntHistogram.
      * 
@@ -22,6 +28,15 @@ public class IntHistogram {
      */
     public IntHistogram(int buckets, int min, int max) {
     	// some code goes here
+        this.min = min;
+        this.max = max;
+        this.width = (int)Math.ceil ( (max - min + 1) / (double)buckets );
+        this.ntups = 0;
+        this.histogram = new int[buckets];
+    }
+
+    private int getBucket(int v) {
+        return Math.max((v - this.min), 0) / width;
     }
 
     /**
@@ -30,6 +45,9 @@ public class IntHistogram {
      */
     public void addValue(int v) {
     	// some code goes here
+        int bucket = this.getBucket(v);
+        this.histogram[bucket]++;
+        this.ntups++;
     }
 
     /**
@@ -44,10 +62,57 @@ public class IntHistogram {
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
 
-    	// some code goes here
+        switch (op) {
+            case EQUALS:
+                if (v > this.max || v < this.min) { return 0.0; }
+                return equalsSelectivity(v);
+            case NOT_EQUALS:
+                if (v > this.max || v < this.min) { return 1.0; }
+                return 1.0 - equalsSelectivity(v);
+            case GREATER_THAN:
+                if (v < this.min) { return 1.0; }
+                if (v >= this.max) { return 0.0; }
+                return greaterThanSelectivity(v);
+            case LESS_THAN_OR_EQ:
+                if (v < min) { return 0.0; }
+                if (v > max) { return 1.0; }
+                return 1.0 - greaterThanSelectivity(v);
+            case LESS_THAN:
+                if (v <= this.min) { return 0.0; }
+                if (v > this.max) { return 1.0; }
+                return 1.0 - greaterThanOrEqualSelectivity(v);
+            case GREATER_THAN_OR_EQ:
+                if (v < min) { return 1.0; }
+                if (v > max) { return 0.0; }
+                return greaterThanOrEqualSelectivity(v);
+        }
+
+        // Should never reach here.
         return -1.0;
     }
-    
+
+    private double equalsSelectivity(int v) {
+        int bucket = this.getBucket(v);
+        return ((double) this.histogram[bucket] / this.width) / this.ntups;
+    }
+
+    private double greaterThanSelectivity(int v) {
+        int bucket = this.getBucket(v);
+        double b_f = (double) this.histogram[bucket] / this.ntups;
+        double b_part = (double) ((this.width * bucket) - (v - this.min)) / this.width;
+        
+        double selectivity = b_f * b_part;
+        for (int i = bucket + 1; i < this.histogram.length; i++) {
+            selectivity += (double) this.histogram[i] / this.ntups;
+        }
+
+        return selectivity;
+    }
+
+    private double greaterThanOrEqualSelectivity(int v) {
+        return equalsSelectivity(v) + greaterThanSelectivity(v);
+    }
+
     /**
      * @return
      *     the average selectivity of this histogram.
@@ -66,8 +131,18 @@ public class IntHistogram {
      * @return A string describing this histogram, for debugging purposes
      */
     public String toString() {
-
         // some code goes here
-        return null;
+        String buckets = "[ ";
+        for(int i = 0; i < histogram.length-1; i++){
+            buckets += this.histogram[i] + " | ";
+        }
+        buckets += this.histogram[histogram.length-1] + " ]";
+
+        String result = "min: " + this.min + "\n" +
+                        "max: " + this.max + "\n" +
+                        "width: " + this.width + "\n" +
+                        "ntups: " + this.ntups + "\n" +
+                        "histogram: " + buckets;
+        return result;
     }
 }
