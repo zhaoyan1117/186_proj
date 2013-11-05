f3e91eafb96fce8832a11f81eaaf8d823a71e31a
Yan Zhao <zhaoyan1117@gmail.com>
2013-11-04 22:02:53 -0800

    Exercise 4 tests passed.

--- src/java/simpledb/JoinOptimizer.java
+++ src/java/simpledb/JoinOptimizer.java
@@ -111,7 +111,9 @@ public class JoinOptimizer {
             // HINT: You may need to use the variable "j" if you implemented
             // a join algorithm that's more complicated than a basic nested-loops
             // join.
-            return -1.0;
+            double io_cost = cost1 + card1 * cost2;
+            double cpu_cost = card1 * card2;
+            return io_cost + cpu_cost;
         }
     }
 
@@ -154,9 +156,27 @@ public class JoinOptimizer {
             String field2PureName, int card1, int card2, boolean t1pkey,
             boolean t2pkey, Map<String, TableStats> stats,
             Map<String, Integer> tableAliasToId) {
-        int card = 1;
         // some code goes here
-        return card <= 0 ? 1 : card;
+        switch (joinOp) {
+            case LIKE:  
+            case EQUALS: {
+                    if (t1pkey) {
+                        return card2;
+                    } else if (t2pkey) {
+                        return card1;
+                    } else {
+                        return Math.max(card1, card2);
+                    } 
+            }
+            case LESS_THAN:
+            case GREATER_THAN:
+            case LESS_THAN_OR_EQ:
+            case GREATER_THAN_OR_EQ:
+            case NOT_EQUALS:
+                return (int) (card1 * card2 * .3);
+            default:
+                return 0;
+        }
     }
 
     /**
